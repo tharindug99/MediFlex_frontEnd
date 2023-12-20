f06ae392b297965d24ebd1b3768ff2810baf4335
tharindug99 <tngodage@std.appsc.sab.ac.lk>
2023-12-20 08:33:09 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['@mui/material'],
-    include: ['@mui/icons-material'],
-  },
   // base:"/MediFlex_frontEnd"
 })
